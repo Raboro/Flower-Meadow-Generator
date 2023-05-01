97a911efe8de13899afdb85e9114a2e445554757
raboro <raborogit@gmail.com>
2023-05-01 17:48:58 +0200

    added flower model

---
+++ flower-meadow-generator-backend/src/main/java/io/github/raboro/flowermeadowgenerator/database/model/Flower.java
@@ -0,0 +1,63 @@
+package io.github.raboro.flowermeadowgenerator.database.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+/**
+ * @author Raboro
+ */
+@Entity
+public class Flower implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private long id;
+    private String name;
+    private byte[] image;
+
+    public Flower() {
+
+    }
+
+    public Flower(long id, String name, byte[] image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Flower{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", image=" + Arrays.toString(image) +
+                '}';
+    }
+}
