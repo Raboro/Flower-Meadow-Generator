c73b5165c41fb49e3815cb96acd386a036cfd4a9
Raboro <marius.woerfelreal@gmail.com>
2023-05-02 17:08:53 +0200

    added component scan to flower repo

--- flower-meadow-generator-backend/src/main/java/io/github/raboro/flowermeadowgenerator/database/repository/FlowerRepository.java
+++ flower-meadow-generator-backend/src/main/java/io/github/raboro/flowermeadowgenerator/database/repository/FlowerRepository.java
@@ -1,6 +1,7 @@
 package io.github.raboro.flowermeadowgenerator.database.repository;
 
 import io.github.raboro.flowermeadowgenerator.database.model.Flower;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Repository;
  * @author Raboro
  */
 @Repository
+@ComponentScan(basePackages = { "io.github.raboro.flowermeadowgenerator.businesslogic"})
 public interface FlowerRepository extends JpaRepository<Flower, Long> {
 }
