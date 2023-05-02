219dcfbd558bb67b4e880bea68eb557e9268ac93
Raboro <marius.woerfelreal@gmail.com>
2023-05-02 19:02:04 +0200

    used lombok to replace duplicated code

--- flower-meadow-generator-backend/src/main/java/io/github/raboro/flowermeadowgenerator/database/model/Flower.java
+++ flower-meadow-generator-backend/src/main/java/io/github/raboro/flowermeadowgenerator/database/model/Flower.java
@@ -1,6 +1,8 @@
 package io.github.raboro.flowermeadowgenerator.database.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
@@ -8,6 +10,8 @@ import java.io.Serializable;
  * @author Raboro
  */
 @Entity
+@Data
+@NoArgsConstructor
 public class Flower implements Serializable {
 
     @Id
@@ -25,10 +29,6 @@ public class Flower implements Serializable {
     private short petalWidth;
     private boolean petalThrones;
 
-    public Flower() {
-
-    }
-
     public Flower(String name, String category, String stemColor, short stemHeight, short stemWidth, boolean stemThrones, String petalColor, short petalHeight, short petalWidth, boolean petalThrones) {
         this.name = name;
         this.category = category;
@@ -40,100 +40,5 @@ public class Flower implements Serializable {
         this.petalHeight = petalHeight;
         this.petalWidth = petalWidth;
         this.petalThrones = petalThrones;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getStemColor() {
-        return stemColor;
-    }
-
-    public void setStemColor(String stemColor) {
-        this.stemColor = stemColor;
-    }
-
-    public short getStemHeight() {
-        return stemHeight;
-    }
-
-    public void setStemHeight(short stemHeight) {
-        this.stemHeight = stemHeight;
-    }
-
-    public short getStemWidth() {
-        return stemWidth;
-    }
-
-    public void setStemWidth(short stemWidth) {
-        this.stemWidth = stemWidth;
-    }
-
-    public boolean isStemThrones() {
-        return stemThrones;
-    }
-
-    public void setStemThrones(boolean stemThrons) {
-        this.stemThrones = stemThrons;
-    }
-
-    public String getPetalColor() {
-        return petalColor;
-    }
-
-    public void setPetalColor(String petalColor) {
-        this.petalColor = petalColor;
-    }
-
-    public short getPetalHeight() {
-        return petalHeight;
-    }
-
-    public void setPetalHeight(short petalHeight) {
-        this.petalHeight = petalHeight;
-    }
-
-    public short getPetalWidth() {
-        return petalWidth;
-    }
-
-    public void setPetalWidth(short petalWidth) {
-        this.petalWidth = petalWidth;
-    }
-
-    public boolean isPetalThrones() {
-        return petalThrones;
-    }
-
-    public void setPetalThrones(boolean petalThrons) {
-        this.petalThrones = petalThrons;
-    }
-
-    @Override
-    public String toString() {
-        return ("Flower{id=%d, name='%s', stemColor='%s', stemHeight=%s, stemWidth=%s, stemThrones=%s, petalColor='%s', petalHeight=%s, petalWidth=%s, " +
-                "petalThrones=%s}")
-                .formatted(id, name, stemColor, stemHeight, stemWidth, stemThrones, petalColor, petalHeight, petalWidth, petalThrones);
     }
 }
--- flower-meadow-generator-backend/src/main/java/io/github/raboro/flowermeadowgenerator/rest/dto/FlowerDTO.java
+++ flower-meadow-generator-backend/src/main/java/io/github/raboro/flowermeadowgenerator/rest/dto/FlowerDTO.java
@@ -1,8 +1,13 @@
 package io.github.raboro.flowermeadowgenerator.rest.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Raboro
  */
+@Data
+@NoArgsConstructor
 public class FlowerDTO {
 
     long id;
@@ -17,10 +22,6 @@ public class FlowerDTO {
     private short petalWidth;
     private boolean petalThrones;
 
-    public FlowerDTO() {
-
-    }
-
     public FlowerDTO(long id, String name, String category, String stemColor, short stemHeight, short stemWidth, boolean stemThrones, String petalColor, short petalHeight, short petalWidth, boolean petalThrones) {
         this.id = id;
         this.name = name;
@@ -33,100 +34,5 @@ public class FlowerDTO {
         this.petalHeight = petalHeight;
         this.petalWidth = petalWidth;
         this.petalThrones = petalThrones;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getStemColor() {
-        return stemColor;
-    }
-
-    public void setStemColor(String stemColor) {
-        this.stemColor = stemColor;
-    }
-
-    public short getStemHeight() {
-        return stemHeight;
-    }
-
-    public void setStemHeight(short stemHeight) {
-        this.stemHeight = stemHeight;
-    }
-
-    public short getStemWidth() {
-        return stemWidth;
-    }
-
-    public void setStemWidth(short stemWidth) {
-        this.stemWidth = stemWidth;
-    }
-
-    public boolean isStemThrones() {
-        return stemThrones;
-    }
-
-    public void setStemThrones(boolean stemThrones) {
-        this.stemThrones = stemThrones;
-    }
-
-    public String getPetalColor() {
-        return petalColor;
-    }
-
-    public void setPetalColor(String petalColor) {
-        this.petalColor = petalColor;
-    }
-
-    public short getPetalHeight() {
-        return petalHeight;
-    }
-
-    public void setPetalHeight(short petalHeight) {
-        this.petalHeight = petalHeight;
-    }
-
-    public short getPetalWidth() {
-        return petalWidth;
-    }
-
-    public void setPetalWidth(short petalWidth) {
-        this.petalWidth = petalWidth;
-    }
-
-    public boolean isPetalThrones() {
-        return petalThrones;
-    }
-
-    public void setPetalThrones(boolean petalThrones) {
-        this.petalThrones = petalThrones;
-    }
-
-    @Override
-    public String toString() {
-        return ("FlowerDTO{id=%d, name='%s', stemColor='%s', stemHeight=%s, stemWidth=%s, stemThrones=%s, petalColor='%s', petalHeight=%s, petalWidth=%s, " +
-                "petalThrones=%s}")
-                .formatted(id, name, stemColor, stemHeight, stemWidth, stemThrones, petalColor, petalHeight, petalWidth, petalThrones);
     }
 }
