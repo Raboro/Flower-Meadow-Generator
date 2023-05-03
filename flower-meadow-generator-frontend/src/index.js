760de0869d1a7d523f246369b02b908331d92216
Raboro <marius.woerfelreal@gmail.com>
2023-05-03 22:50:07 +0200

    introduces css and js file

---
+++ flower-meadow-generator-frontend/src/index.js
@@ -0,0 +1,10 @@
+function test() {
+    fetch('http://localhost:8081/flower')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
