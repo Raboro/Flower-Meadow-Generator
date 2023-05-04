a3bedc15d79c5f6db9673553397ab7e44412f559
Raboro <marius.woerfelreal@gmail.com>
2023-05-04 11:44:00 +0200

    added test and skip test in dockerfile

---
+++ flower-meadow-generator-backend/src/test/java/io/github/raboro/flowermeadowgenerator/FlowerMeadowGeneratorBackendApplicationTest.java
@@ -0,0 +1,13 @@
+package io.github.raboro.flowermeadowgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerMeadowGeneratorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
