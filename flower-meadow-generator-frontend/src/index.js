4abb01e47523e3aa4df7b83846925c9ca7310377
Raboro <marius.woerfelreal@gmail.com>
2023-05-05 16:31:55 +0200

    added flower info fetching if adding & formatted

--- flower-meadow-generator-frontend/src/index.js
+++ flower-meadow-generator-frontend/src/index.js
@@ -9,6 +9,43 @@ function toggleAddFlower() {
 }
 
 // eslint-disable-next-line no-unused-vars
+function addFlower() {
+    const flower = createFlower();
+    if (isInvalid(flower)) {
+        handleInvalidFlower();
+        return;
+    }
+    sendValidFlower();
+}
+
+function createFlower() {
+    return {
+        "name": document.getElementById("addFlowerName").textContent,
+        "category": document.getElementById("addFlowerCategory").textContent,
+        "stemColor": document.getElementById("addFlowerStemColor").value,
+        "stemHeight": document.getElementById("addFlowerStemHeight").value,
+        "stemWidth": document.getElementById("addFlowerStemWidth").value,
+        "stemThrones": document.getElementById("addFlowerStemThrones").checked,
+        "PetalColor": document.getElementById("addFlowerPetalColor").value,
+        "PetalHeight": document.getElementById("addFlowerPetalHeight").value,
+        "PetalWidth": document.getElementById("addFlowerPetalWidth").value,
+        "PetalThrones": document.getElementById("addFlowerPetalThrones").checked,
+    };
+}
+
+function isInvalid(flower) {
+    // implement
+}
+
+function handleInvalidFlower(flower) {
+    // implement
+}
+
+function sendValidFlower(flower) {
+    // implement
+}
+
+// eslint-disable-next-line no-unused-vars
 function test() {
     fetch('http://localhost:8081/flower')
         .then(response => response.json())
