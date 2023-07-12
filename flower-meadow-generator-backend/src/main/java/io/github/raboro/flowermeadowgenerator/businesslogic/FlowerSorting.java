69e8ec817a9b40d44dfe687c9c530729cbd4cbc9
Raboro <marius.woerfelreal@gmail.com>
2023-07-12 21:11:01 +0200

    added sorting for stem/petal color

--- flower-meadow-generator-backend/src/main/java/io/github/raboro/flowermeadowgenerator/businesslogic/FlowerSorting.java
+++ flower-meadow-generator-backend/src/main/java/io/github/raboro/flowermeadowgenerator/businesslogic/FlowerSorting.java
@@ -2,6 +2,8 @@ package io.github.raboro.flowermeadowgenerator.businesslogic;
 
 import io.github.raboro.flowermeadowgenerator.rest.dto.FlowerDTO;
 
+import java.awt.*;
+import java.util.List;
 import java.util.*;
 import java.util.function.Consumer;
 
@@ -57,12 +59,31 @@ enum FlowerSorting {
         Consumer<List<FlowerDTO>> sort() {
             return flowerDTOS -> flowerDTOS.sort((o1, o2) -> Boolean.compare(o1.isPetalThrones(), o2.isPetalThrones()));
         }
+    },
+    STEMCOLOR {
+        @Override
+        Consumer<List<FlowerDTO>> sort() {
+            return flowerDTOS -> flowerDTOS.sort((f1, f2) -> compareColor(f1.getStemColor(), f2.getStemColor()));
+
+        }
+    },
+    PETALCOLOR {
+        @Override
+        Consumer<List<FlowerDTO>> sort() {
+            return flowerDTOS -> flowerDTOS.sort((f1, f2) -> compareColor(f1.getPetalColor(), f2.getPetalColor()));
+        }
     };
 
     abstract Consumer<List<FlowerDTO>> sort();
 
     private static String toLowerCase(String s) {
         return s.toLowerCase(Locale.ENGLISH);
+    }
+
+    private static int compareColor(String colorHex1, String colorHex2) {
+        final Color color1 = Color.decode(colorHex1);
+        final Color color2 = Color.decode(colorHex2);
+        return Integer.compare(color1.getRGB(), color2.getRGB());
     }
 
     static Optional<FlowerSorting> getSorting(String name) {
